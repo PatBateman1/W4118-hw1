476fa254410fe7dad4c905ab24f881a905d8f4fc
bateman <zz2700@columbia.edu>
2020-09-14 19:54:14 -0400

    part 2

--- part2/pokedex.c
+++ part2/pokedex.c
@@ -1,6 +1,7 @@
 #include <linux/module.h>
 #include <linux/list.h>
 #include <linux/printk.h>
+#include <linux/slab.h>
 
 struct pokemon {
 	char name[32];
@@ -14,20 +15,37 @@ void print_pokemon(struct pokemon *p)
 }
 
 /* TODO: declare a single static struct list_head, named pokedex */
+static LIST_HEAD(pokedex);
 
 void add_pokemon(char *name, int dex_no)
 {
 	/* TODO: write your code here */
+	struct pokemon *new_pk;
+	new_pk = kmalloc(sizeof(*new_pk), GFP_KERNEL);
+	strcpy(new_pk->name, name);
+	new_pk->dex_no = dex_no;
+	INIT_LIST_HEAD(&new_pk->list);
+	list_add_tail(&new_pk->list, &pokedex);
 }
 
 void print_pokedex(void)
 {
 	/* TODO: write your code here, using print_pokemon() */
+	struct pokemon *pos;
+	list_for_each_entry(pos, &pokedex, list) {
+		print_pokemon(pos);
+	}
 }
 
 void delete_pokedex(void)
 {
 	/* TODO: write your code here */
+	struct list_head *pos;
+	struct list_head *n;
+	list_for_each_safe(pos, n, &pokedex) {
+		list_del(pos);
+		kfree(list_entry(pos, struct pokemon, list));
+	}
 }
 
 int pokedex_init(void)
@@ -58,4 +76,4 @@ module_exit( pokedex_exit );
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("A handy Pokedex module");
-MODULE_AUTHOR("SGG");
+MODULE_AUTHOR("zz2700");
